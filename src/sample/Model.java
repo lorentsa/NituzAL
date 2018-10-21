8638a7e6f05f3a15ad20d15591e9d9f83b76ddea
AMIT MOSHE <amit.moshe100@gmail.com>
2018-10-21 21:29:42 +0300

    added imodel and user model implements imodel

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,5 +1,26 @@
 package sample;
 
-public class Model {
-    int alll;
+import java.util.Date;
+
+public class Model implements IModel{
+
+    @Override
+    public void Create(String userName, String password, String firstName, String lastName, Date bithday, String city) {
+
+    }
+
+    @Override
+    public User Read(String userName) {
+        return null;
+    }
+
+    @Override
+    public void Update(String userName, String password, String firstName, String lastName, Date bithday, String city) {
+
+    }
+
+    @Override
+    public void Delete(String userName) {
+
+    }
 }
