32b3ff978a8bf09fab15a0f304bf76219050cf98
liron refaeli <lironref@post.bgu.ac.il>
2018-10-27 15:09:05 +0300

    Delete function is working

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
 //        this.model = model;
 //    }
 
-    public Alert Create(String usernameS,String passwordS, String confirmS,String firstS,String lastS,String dateS,String cityS){
+    public Alert Create(String usernameS, String passwordS, String confirmS, String firstS, String lastS, String dateS, String cityS) {
         if (usernameS.length() == 0 || passwordS.length() == 0 || confirmS.length() == 0 ||
                 firstS.length() == 0 || lastS.length() == 0 || cityS.length() == 0 || dateS.length() == 0) {
             //if one or more deails aren't filled
@@ -22,16 +22,15 @@ public class Controller {
             errorAlert.setContentText("At least one of the fields are empty.\nFill all fields and try again ");
 //            errorAlert.showAndWait();
             return errorAlert;
-        } else{
-            if(!passwordS.equals(confirmS)){
+        } else {
+            if (!passwordS.equals(confirmS)) {
                 //if passwords dont match
                 Alert errorAlert = new Alert(Alert.AlertType.ERROR);
                 errorAlert.setHeaderText("Input not valid");
                 errorAlert.setContentText("Passwords doesn't match.\nPlease try again.");
 //                errorAlert.showAndWait();
                 return errorAlert;
-            }
-            else {
+            } else {
                 boolean flag = model.Create(usernameS, passwordS, firstS, lastS, dateS, cityS);
                 if (flag) {
                     Alert success = new Alert(Alert.AlertType.CONFIRMATION);
@@ -49,4 +48,37 @@ public class Controller {
             }
         }
     }
+
+
+    public Alert Delete(String DeleteUserNameOutput, String DeletePasswordOutput) {
+        if (DeleteUserNameOutput.length() == 0 || DeletePasswordOutput.length() == 0) {
+            //if one or more deails aren't filled
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Input not valid");
+            errorAlert.setContentText("At least one of the fields are empty.\nFill all fields and try again ");
+            return errorAlert;
+        } else {
+
+
+            boolean flag = model.Delete(DeleteUserNameOutput, DeletePasswordOutput);
+            if (flag) {
+                Alert success = new Alert(Alert.AlertType.CONFIRMATION);
+                success.setHeaderText("Action Succeeded");
+                success.setContentText("New user created successfuly! ");
+//                    success.showAndWait();
+                return success;
+
+            }
+            else
+            {
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("Action failed");
+                errorAlert.setContentText("Doen't success to connect to the DB ");
+                return errorAlert;
+            }
+
+        }
+
+    }
 }
+
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -12,6 +12,25 @@ public class Model implements IModel {
 
     }
 
+    /**
+     * Connect to the Users.db database
+     *
+     * @return the Connection object
+     */
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:Users.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("connect");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
     @Override
     public boolean Create(String userName, String password, String firstName, String lastName, String birthday, String city) {
         String sql = "INSERT INTO Users(user_name,password,first_name,last_name,city,birthdate) VALUES(?,?,?,?,?,?)";
@@ -37,9 +56,9 @@ public class Model implements IModel {
         return null;
     }
 
-
     /**
      * the function updates the details of an exists user
+     *
      * @param userName
      * @param password
      * @param firstName
@@ -47,10 +66,12 @@ public class Model implements IModel {
      * @param birthday
      * @param city
      */
+
+
     @Override
     public void Update(String userName, String password, String firstName, String lastName, String birthday, String city) {
         String sql = "UPDATE Users SET user_name = ? , " + "password = ? " + "first_name = ?" + "last_name = ?" +
-                "city = ?" + "birthdate = ?" +"WHERE username = ?";
+                "city = ?" + "birthdate = ?" + "WHERE username = ?";
 
         try (Connection conn = this.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -70,78 +91,25 @@ public class Model implements IModel {
     }
 
 
-
-
-
-
-   /**
-     * Connect to the Users.db database
-     *
-     * @return the Connection object
-     */
-
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:Users.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-            System.out.println("connect");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
-/**
-    public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
- **/
     @Override
     /**
      * Delete function get the strings username and password.
      * It checks if the username exists, and also if the password is match.
      * if user was deleted, success alert is shown. if not, the user get error alert.
      */
-    public void Delete(String userName, String Password) {
+    public boolean Delete(String userName, String Password) {
 
-            //String sql = "DELETE FROM Users WHERE user_name =" + userName + " AND password =" + Password;
-            String sql = "DELETE FROM Users WHERE user_name = liron";
-        System.out.println("HI");
-            try (Connection conn = this.connect();
-                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-                // set the username param
-               // pstmt.setString(1, userName);
-                // set the password param
-                //pstmt.setString(2, Password);
-                // execute the delete statement
-
-                System.out.print(sql);
-                pstmt.executeUpdate();
-
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
+        String sql = "DELETE FROM Users WHERE user_name = ? AND password = ?";
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, userName);
+            pstmt.setString(2, Password);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
+        return true;
 
     }
+}
